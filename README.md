bb18ec1629bc44d8fa539d77034e600a94bf9b3b
Skip2Point0 <68402274+Skip2Point0@users.noreply.github.com>
2021-07-15 19:49:43 -0400

    Fixing instructions. adding soreadsheet.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # meraki-api-record-macaddr
+
+Summary:
+
+Purpose of this script is to go through all serial numbers in the spreadsheet, pull mac addresses of those devices, and 
+record them in that spreadsheet.
+
+Requirements:
+
+1) Interpreter: Python 3.9.0+
+2) Python Packages: requests, json, openpyxl
+3) Excel Spreadsheet - .XLSX format
+4) API support for the Organization is enabled in Meraki Dashboard. Admin has generated their custom API key.
+5) Device licensing, and inventory for devices used below, have been claimed under Organization settings.
+
+How to run:
+
+1) Attached is a spreadsheet that can be used. Custom spreadsheet can be used, but column variables must be changed under
+   PARAMETERS section.
+2) Open claim_devices_to_network.py with your favorite text editor and edit PARAMETERS sections of the script:
+    1) Lines 11-16 is mandatory.
+    2) Line 21-23 are required if using custom spreadsheet.
+3) Run python3 claim_devices_to_network.py in your terminal. Ensure spreadsheet file and python script are
+   in the same folder/location.
---
+++ pull_device_macaddresses.py
@@ -0,0 +1,135 @@
+import requests
+import json
+from openpyxl import load_workbook
+
+# ################################################# PARAMETERS BELOW ###################################################
+# Please fill in parameters below.
+# You can add devices to one or multiple networks.
+# ap_abr is a variable that is used to track device names. Typical syntax for NN1-AP1-25 is NN1-
+# Make sure that every parameter is added in correct order: Network Name1 > Sheet Name1 > NN1-
+# ######################################################################################################################
+meraki_api = 'APIKEY'
+organization_id = 'Organization Name'
+network_ids = ['Network Name1', 'Network Name2']
+spread = 'spreadsheet.xlsx'
+tabs = ['Sheet Name1', 'Sheet Name2']
+ap_abr = ['NN1-', 'NN2-']
+# ##################################################### PARAMETERS #####################################################
+# If using default spreadsheet, no changes below are necessary.
+# Custom spreadsheets require column changes below.
+# ######################################################################################################################
+ap_name_column = 'A'
+serials_column = 'D'
+mac_column = 'E'
+# ################################################# PARAMETERS ABOVE ###################################################
+net_dictionary = {}
+headers = {
+    'X-Cisco-Meraki-API-Key': meraki_api,
+    'Content-Type': 'application/json'
+}
+print(spread)
+wb = load_workbook(spread)
+
+
+def pull_organization_id(head):
+    url = "https://api.meraki.com/api/v0/organizations"
+    payload = {}
+
+    response = requests.request("GET", url, headers=head, data=payload)
+    response = response.content
+    response = json.loads(response)
+    for dicti in response:
+        name = dicti["name"]
+        if name == organization_id:
+            org_id = dicti["id"]
+            print("#################################################")
+            print(name + "\n" + "Organization ID: " + org_id)
+            print("#################################################")
+            return org_id
+        else:
+            continue
+
+
+def pull_organization_networks(head):
+    global net_dictionary
+    global organization_id
+
+    organization_id = pull_organization_id(head)
+    url = "https://api.meraki.com/api/v0/organizations/" + organization_id + "/networks"
+    payload = {}
+    response = requests.request("GET", url, headers=head, data=payload)
+    response = response.content
+    json_response = json.loads(response)
+    for networks in json_response:
+        name = networks['name']
+        n_id = networks['id']
+        net_dictionary[name] = n_id
+    print(net_dictionary)
+    return net_dictionary
+
+
+def pull_destination_networks():
+    global network_ids
+
+    dest_network_ids = []
+    for n in network_ids:
+        for i in net_dictionary:
+            if n == i:
+                print("Destination Network: " + n)
+                dest_network_ids.append(net_dictionary[n])
+                break
+            else:
+                continue
+    print(dest_network_ids)
+    return dest_network_ids
+
+
+pull_organization_networks(headers)
+networks_dest = pull_destination_networks()
+
+
+def meraki_ap_parse_mac(workbook, networks, tbs, abr, name, serial, mac, head):
+    ap_index = []
+    print("########################################")
+    print("PARSING FOR MAC AND SAVING...")
+    incr = 0
+    for tab in tbs:
+        sheet = workbook[tab]
+        # unsorted_ap = []
+        row_index = []
+        print("########################################")
+        print("NETWORK:  " + tab)
+        print("########################################")
+
+        incr2 = 1
+        for row in range(sheet.max_row):
+            a = sheet[name + str(incr2)]
+            unsorted_ap = a.value
+            incr2 = incr2 + 1
+
+            if abr[incr] in str(unsorted_ap):
+                n_ap_index = row + 1
+                ap_index.append(unsorted_ap)
+                row_index.append(row)
+                serials = sheet[serial + str(n_ap_index)].value
+
+                url = "https://api.meraki.com/api/v0/networks/" + networks[incr] + "/devices/" + serials
+
+                payload = {
+                    "serial": serials,
+                }
+                payload = json.dumps(payload)
+                response = requests.request("GET", url, headers=head, data=payload)
+                f = response.content
+                json_data = json.loads(f)
+                print(json_data)
+                macs = json_data["mac"]
+                names = json_data["name"]
+                print(names + " : " + macs)
+                sheet[mac + str(n_ap_index)] = macs
+                wb.save(filename=spread)
+
+        incr = incr + 1
+
+
+meraki_ap_parse_mac(wb, networks_dest, tabs, ap_abr, ap_name_column, serials_column, mac_column, headers)
